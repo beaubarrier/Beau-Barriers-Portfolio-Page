0479a68fae4f8341701ff379cc3769ef46ce2132
Beau Barrier <beaubarrier@gmail.com>
2021-07-25 18:56:06 -0700

    Added images, added new functionality to contact page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,17 +17,21 @@ let instaIcon = $("#instaIcon");
 let emailIconDiv = $("#emailIconDiv");
 let githubLeafRight = $("#githubLeafRight");
 let githubLeafLeft = $("#githubLeafLeft");
+let instaTxtDisplay = $("#instaTxtDisplay");
+let emailTxtDisplay = $("#emailTxtDisplay");
+let linkedInTxtDisplay = $("#linkedInTxtDisplay");
+let reachOut = $("#reachOut");
 
 
-myName.on("click", function myNameFunc(event) {
-    event.preventDefault();
+myName.on("click", function myNameFunc(e) {
+    e.preventDefault();
     portrait.css("display", "initial");
     pages.css("display", "none");
     contentBG.css("display", "none");
-})
+});
 
-contact.on("click", function contactPageFunc(event) {
-    event.preventDefault();
+contact.on("click", function contactPageFunc(e) {
+    e.preventDefault();
 
     contactPage.css("display", "initial");
     contentBG.css("display", "initial");
@@ -36,10 +40,11 @@ contact.on("click", function contactPageFunc(event) {
     myStoryPage.css("display", "none");
     githubPage.css("display", "none");
 
-})
+});
 
-myStory.on("click", function myStoryFunc(event) {
-    event.preventDefault();
+
+myStory.on("click", function myStoryFunc(e) {
+    e.preventDefault();
 
     myStoryPage.css("display", "initial");
     contentBG.css("display", "initial");
@@ -47,10 +52,11 @@ myStory.on("click", function myStoryFunc(event) {
     portrait.css("display", "none");
     githubPage.css("display", "none");
     contactPage.css("display", "none");
-})
+});
 
-github.on("click", function githubFunc(event) {
-    event.preventDefault();
+
+github.on("click", function githubFunc(e) {
+    e.preventDefault();
 
     githubPage.css("display", "initial");
     contentBG.css("display", "initial");
@@ -58,7 +64,47 @@ github.on("click", function githubFunc(event) {
     portrait.css("display", "none");
     myStoryPage.css("display", "none");
     contactPage.css("display", "none");
-})
+});
 
 
+emailIcon.mouseenter(function showEmailTxt(e) {
+    e.preventDefault();
 
+    emailTxtDisplay.css("display", "initial")
+    reachOut.css("display", "none")
+
+});
+emailIcon.mouseleave(function showEmailTxt(e) {
+    e.preventDefault();
+    reachOut.css("display", "initial")
+    emailTxtDisplay.css("display", "none")
+
+});
+
+
+linkedInIcon.mouseenter(function showEmailTxt(e) {
+    e.preventDefault();
+    reachOut.css("display", "none")
+    linkedInTxtDisplay.css("display", "initial")
+
+});
+linkedInIcon.mouseleave(function showEmailTxt(e) {
+    e.preventDefault();
+    reachOut.css("display", "initial")
+    linkedInTxtDisplay.css("display", "none")
+
+});
+
+
+instaIcon.mouseenter(function showEmailTxt(e) {
+    e.preventDefault();
+    reachOut.css("display", "none")
+    instaTxtDisplay.css("display", "initial")
+
+});
+instaIcon.mouseleave(function showEmailTxt(e) {
+    e.preventDefault();
+    reachOut.css("display", "initial")
+    instaTxtDisplay.css("display", "none")
+
+});
